b23bb1026993cd515bb1f7199751c8a498a5dbbf
hugopassos <hugo.passos@hotmail.com>
2019-12-18 17:46:20 -0300

    Added remember_token to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
+  before_create :create_remember_token
+
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end

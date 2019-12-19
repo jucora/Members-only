4488c55dd1fb000d9368b51b8dd5cc543184786c
hugopassos <hugo.passos@hotmail.com>
2019-12-19 13:31:12 -0300

    Added logic to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,11 @@ class PostsController < ApplicationController
 
   private
 
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
+
   def member?
-		current_user.member
+		current_user
 	end
 end

fa182bfb2082c7063c3f526aff33bb47357aa16c
hugopassos <hugo.passos@hotmail.com>
2020-01-06 13:30:59 -0300

    Created the users controller and new and create methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/create'
   get 'posts/new'
   get 'posts/create'
   post 'posts/create'
   get 'posts/index'
   root 'sessions#new'
-  # get 'signup', to:'users#new'
+  get 'signup', to:'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#delete'
